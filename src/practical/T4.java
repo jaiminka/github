e685c2eaa95e6c0e9d7eb35dc05b4d8cd6a18e0b
upal-patel <patelupalm@gmail.com>
2021-03-20 16:31:35 -0400

    upal from upal

--- src/practical/T4.java
+++ src/practical/T4.java
@@ -5,9 +5,6 @@ import org.testng.annotations.Test;
 public class T4 {
   @Test
   public void test6() {
-
-	System.out.println("test6");
-	  System.out.println("hello Upal");
-		System.out.println("hello Nidhi");
+upal from upal
 
 }
