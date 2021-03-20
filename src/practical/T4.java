b3b3a027136331b90c05efecaa9b790fe2397f30
jaiminka <kachadiyajaimin@gmail.com>
2021-03-20 13:55:36 -0400

    t4 change from jaimin

--- src/practical/T4.java
+++ src/practical/T4.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 public class T4 {
   @Test
   public void test6() {
-	  System.out.println("test6");
+	System.out.println("hello jaimin");  
+	System.out.println("test6");
   }
 }
