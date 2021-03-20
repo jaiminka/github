0e2ecd0b4e145aa6bede32e884f634d0f3bb2c29
upal-patel <patelupalm@gmail.com>
2021-03-20 15:21:02 -0400

    1

--- src/practical/T4.java
+++ src/practical/T4.java
@@ -6,12 +6,10 @@ public class T4 {
   @Test
   public void test6() {
 
-	System.out.println("hello jaimin");  
 	System.out.println("test6");
 	  System.out.println("hello Upal");
 
 
-	System.out.println("new hello jaimin");
 
   }
 }
